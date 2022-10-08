7cae7c7824d528b7b367ff46a54d0ef46a7305e9
art.antoshin <art.antoshin1992@gmail.com>
2022-10-09 01:55:44 +0400

    Начальный этап работы

--- IfElseWhile/Program.cs
+++ IfElseWhile/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+int num1 = 8;
+int num2 = 6;
+Encoding cp1251 = Encoding.GetEncoding(1252);
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
+else
+{
+    Console.WriteLine("Число num1 равно числу num2");
+}
