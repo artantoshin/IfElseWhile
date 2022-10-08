e623b8961cf2407c97ec2c15571f2750525d5da2
art.antoshin <art.antoshin1992@gmail.com>
2022-10-09 02:01:52 +0400

    3 коммит

--- IfElseWhile/Program.cs
+++ IfElseWhile/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Hello, World!");
 
 int num1 = 8;
 int num2 = 6;
-Encoding cp1251 = Encoding.GetEncoding(1252);
+Encoding cp1251 = Encoding.GetEncoding(1251);
 if (num1 > num2)
 {
     Console.WriteLine($"Число {num1} больше числа {num2}");
@@ -18,3 +18,9 @@ else
 {
     Console.WriteLine("Число num1 равно числу num2");
 }
+
+int x = 3;
+int y = 2;
+
+int z = x < y ? (x + y) : (x - y);
+Console.WriteLine(z);   // 1
